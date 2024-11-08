1a22c04716939b973f496693d2bb86af65c41a7d
Johnny <johnnyzy.huang@mail.utoronto.ca>
2024-11-08 13:38:56 -0800

    updated source_arn to correct path

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -30,8 +30,7 @@ resource "aws_lambda_permission" "main" {
     action = "lambda:InvokeFunction"
     function_name = aws_lambda_function.name.function_name
     principal = "apigateway.amazonaws.com"
-    source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountID}:${aws_api_gateway_rest_api.main.id}/*/${aws_api_gateway_method.main.http_method}${aws_api_gateway_resource.main.path}"
-  
+    source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountID}:${aws_api_gateway_rest_api.main.id}/*/${aws_api_gateway_method.main.http_method}/visitor_counter"
 }
 
 resource "aws_api_gateway_deployment" "main" {
