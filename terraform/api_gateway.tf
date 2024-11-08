f2a5e5194a54ed620b42ee0df5f1d02307d1480b
Johnny <johnnyzy.huang@mail.utoronto.ca>
2024-11-08 15:44:04 -0800

    Updated api_gateway to account for CORS

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -61,3 +61,53 @@ resource "aws_api_gateway_stage" "main" {
   rest_api_id   = aws_api_gateway_rest_api.main.id
   stage_name    = "prod"
 }
+
+# Added to address CORS issues between browser and API
+
+# Define the OPTIONS method for CORS preflight
+resource "aws_api_gateway_method" "options" {
+    rest_api_id = aws_api_gateway_rest_api.main.id
+    resource_id = aws_api_gateway_resource.main.id
+    http_method = "OPTIONS"
+    authorization = "NONE"
+}
+
+# Use a mock integration for the OPTIONS method
+resource "aws_api_gateway_integration" "options_integration" {
+    rest_api_id = aws_api_gateway_rest_api.main.id
+    resource_id = aws_api_gateway_resource.main.id
+    http_method = aws_api_gateway_method.options.http_method
+    type = "MOCK"
+
+    request_templates = {
+        "application/json" = "{\"statusCode\": 200}"
+    }
+}
+
+# Define the method response for OPTIONS, including CORS headers
+resource "aws_api_gateway_method_response" "options_response" {
+    rest_api_id = aws_api_gateway_rest_api.main.id
+    resource_id = aws_api_gateway_resource.main.id
+    http_method = aws_api_gateway_method.options.http_method
+    status_code = "200"
+
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = true
+        "method.response.header.Access-Control-Allow-Methods" = true
+        "method.response.header.Access-Control-Allow-Origin"  = true
+    }
+}
+
+# Define the integration response for the OPTIONS method to include CORS headers
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+    rest_api_id = aws_api_gateway_rest_api.main.id
+    resource_id = aws_api_gateway_resource.main.id
+    http_method = aws_api_gateway_method.options.http_method
+    status_code = aws_api_gateway_method_response.options_response.status_code
+
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+        "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
+        "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    }
+}
