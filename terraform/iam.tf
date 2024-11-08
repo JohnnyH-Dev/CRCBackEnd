931ca590e5892384462a043643c2300cbbffaf3e
Johnny Huang <johnnyhuang632402@gmail.com>
2024-11-08 02:08:25 -0800

    updated assume role policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "assume_role" {
             identifiers = [ "lambda.amazonaws.com" ]
       }
 
-      actions = ["sts: AssumeRole"]
+      actions = ["sts:AssumeRole"]
     }
   
 }
