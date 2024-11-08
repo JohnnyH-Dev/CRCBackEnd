ec8607d35a8001e82e71b8ed237a110707e259fa
Johnny <johnnyzy.huang@mail.utoronto.ca>
2024-11-08 11:54:56 -0800

    added dynamodb resources block for initial visitor count

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -8,3 +8,15 @@ resource "aws_dynamodb_table" "main" {
       type = "S"
     }
 }
+
+resource "aws_dynamodb_table_item" "initial_visitor_count" {
+  table_name = aws_dynamodb_table.main.name
+  hash_key = "counter_ID"
+
+  item = <<ITEM
+  {
+    "counter_ID": {"S": "visitor_counter"},
+    "visitor_count": {"N": "0"}
+  }
+  ITEM
+}
