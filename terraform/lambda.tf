a7e8a88d10db538c199653b69e11064a65aafe05
Johnny Huang <johnnyhuang632402@gmail.com>
2024-11-08 01:25:00 -0800

    Created the lambda.tf file

---
+++ terraform/lambda.tf
@@ -0,0 +1,14 @@
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_dir = "${path.module}/../lambda_function"
+  output_path = "${path.module}/../lambda_function/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "name" {
+    filename = data.archive_file.lambda.output_path
+    function_name = "VisitorCounterFunction"
+    role = aws_iam_role.iam_for_lambda.arn
+    handler = "lambda_function.lambda_handler"
+    runtime = "python3.9"
+
+}
